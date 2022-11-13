f520480a49d8deef26d5a75b6a48786c023cd199
jorgerj22 <103084509+jorgerj22@users.noreply.github.com>
2022-11-13 20:03:32 -0300

    agregros para la pagina

--- mail/contact.php
+++ mail/contact.php
@@ -1,5 +1,5 @@
 <?php
-if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+(empty($_POST['name'])  empty($_POST['subject'])  empty($_POST['message'])  !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
   http_response_code(500);
   exit();
 }
@@ -9,12 +9,12 @@ $email = strip_tags(htmlspecialchars($_POST['email']));
 $m_subject = strip_tags(htmlspecialchars($_POST['subject']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
-$to = "info@example.com"; // Change this email to your //
+$to = "info@arizonacontenidos.com"; // Change this email to your //
 $subject = "$m_subject:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
 $header = "From: $email";
 $header .= "Reply-To: $email";	
 
-if(!mail($to, $subject, $body, $header))
+(mail($to, $subject, $body, $header))
   http_response_code(500);
 ?>
